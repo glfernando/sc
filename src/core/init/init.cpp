e5ee26ccd36ff03bd17061f2b49385842c9d7bc2
Fernando Lugo <lugo.fernando@gmail.com>
2020-12-28 18:48:35 -0800

    core: init: add support for init array
    Signed-off-by: Fernando Lugo <lugo.fernando@gmail.com>

--- src/core/init/init.cpp
+++ src/core/init/init.cpp
@@ -10,11 +10,22 @@ import lib.heap;
 
 using sc::board::debug::uart::puts;
 
+extern void (*__init_array_start[])();
+extern void (*__init_array_end[])();
+
+void init_array()
+{
+    for (void (**f)() = __init_array_start; f < __init_array_end; ++f)
+        (*f)();
+}
+
 extern "C" [[noreturn]] void init()
 {
     sc::board::init();
 
     sc::lib::heap::init();
+
+    init_array();
 
     puts("Welcome to SC\n");
     for (;;) {}
