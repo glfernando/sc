cdd02c800f86fb55133e21089f119e63487870ed
Fernando Lugo <fernando@gmail.com>
2021-01-01 02:22:33 -0800

    lib: add support for C malloc functions
    Use lib.heap module to provide C malloc functions. Create corresponding
    stdlib.h header under libc
    
    Signed-off-by: Fernando Lugo <fernando@gmail.com>

---
+++ src/lib/heap-malloc.cpp
@@ -0,0 +1,22 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright (c) 2021 Fernando Lugo <lugo.fernando@gmail.com>
+ */
+
+#include <stdlib.h>
+#include <string.h>
+
+import lib.heap;
+
+extern "C" void* malloc(size_t size) { return sc::lib::heap::alloc(size); }
+
+extern "C" void* calloc(size_t nmemb, size_t size)
+{
+    void* ptr = malloc(nmemb * size);
+    if (ptr)
+        memset(ptr, 0, nmemb * size);
+    return ptr;
+}
+
+extern "C" void free(void* p) { return sc::lib::heap::free(p); }
