1fa8f93be926925e8026a1285065866fbed6d736
Fernando Lugo <fernando@gmail.com>
2021-01-01 05:04:12 -0800

    libcxx: new: throw exception for no memory
    Signed-off-by: Fernando Lugo <fernando@gmail.com>

--- src/libcxx/new.cpp
+++ src/libcxx/new.cpp
@@ -4,17 +4,22 @@
  * Copyright (c) 2020 Fernando Lugo <lugo.fernando@gmail.com>
  */
 
+#include <errcodes.h>
+
 import lib.heap;
+import lib.exception;
 
 #include <stddef.h>
 
 using sc::lib::heap::alloc;
 using sc::lib::heap::free;
+using sc::lib::exception::exception;
 
 void* operator new(size_t size)
 {
-    // TODO: throw exception once they are available
-    return alloc(size);
+    void* ptr = alloc(size);
+    if (!ptr) throw exception("new failed", ERR_NO_MEMORY);
+    return ptr;
 }
 
 void operator delete(void* p) noexcept { free(p); }
