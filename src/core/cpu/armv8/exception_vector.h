cef4107792b0d10380b2eff3883df0bb47e41162
Fernando Lugo <fernando@gmail.com>
2021-01-09 01:15:54 -0800

    core: cpu: arvm8: add support for CPU exceptions
    Signed-off-by: Fernando Lugo <fernando@gmail.com>

---
+++ src/core/cpu/armv8/exception_vector.h
@@ -0,0 +1,25 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright (C) 2021 Fernando Lugo <lugo.fernando@gmail.com>
+ */
+
+#pragma once
+
+#define EXCEPTION_TYPE_SP0_SYNC 0
+#define EXCEPTION_TYPE_SP0_IRQ  1
+#define EXCEPTION_TYPE_SP0_FIQ  2
+#define EXCEPTION_TYPE_SP0_SERR 3
+#define EXCEPTION_TYPE_SPX_SYNC 4
+#define EXCEPTION_TYPE_SPX_IRQ  5
+#define EXCEPTION_TYPE_SPX_FIQ  6
+#define EXCEPTION_TYPE_SPX_SERR 7
+#define EXCEPTION_TYPE_A64_SYNC 8
+#define EXCEPTION_TYPE_A64_IRQ  9
+#define EXCEPTION_TYPE_A64_FIQ  10
+#define EXCEPTION_TYPE_A64_SERR 11
+#define EXCEPTION_TYPE_A32_SYNC 12
+#define EXCEPTION_TYPE_A32_IRQ  13
+#define EXCEPTION_TYPE_A32_FIQ  14
+#define EXCEPTION_TYPE_A32_SERR 15
+#define EXCEPTION_TYPE_MAX      16
