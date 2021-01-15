75d8cc93374be8b1b2bc5e12e25e45f8d8f02fe7
Fernando Lugo <flugo@google.com>
2021-01-14 19:46:12 -0800

    libcxx: implement aligned new operator
    Signed-off-by: Fernando Lugo <flugo@google.com>

--- src/libcxx/new.cpp
+++ src/libcxx/new.cpp
@@ -14,8 +14,14 @@ using sc::lib::exception::exception;
 using sc::lib::heap::alloc;
 using sc::lib::heap::free;
 
+namespace std {
+
+enum class align_val_t : size_t {};
+
+}
+
 void* operator new(size_t size) {
-    void* ptr = alloc(size);
+    void* ptr = alloc(size, __STDCPP_DEFAULT_NEW_ALIGNMENT__);
     if (!ptr)
         throw exception("new failed", ERR_NO_MEMORY);
     return ptr;
@@ -24,12 +30,34 @@ void* operator new(size_t size) {
 void operator delete(void* p) noexcept {
     free(p);
 }
+
 void* operator new[](size_t size) {
     return operator new(size);
 }
+
 void operator delete[](void* p) noexcept {
     operator delete(p);
 }
+
 void* operator new(size_t, void* p) {
     return p;
+}
+
+void* operator new(size_t size, std::align_val_t align) {
+    void* ptr = alloc(size, static_cast<size_t>(align));
+    if (!ptr)
+        throw exception("new failed", ERR_NO_MEMORY);
+    return ptr;
+}
+
+void* operator new[](size_t size, std::align_val_t align) {
+    return operator new(size, align);
+}
+
+void operator delete(void* p, std::align_val_t align) noexcept {
+    free(p);
+}
+
+void operator delete[](void* p, std::align_val_t align) noexcept {
+    free(p);
 }
