446cacbcce9c026b6c19918d24a0a6b390b5df6f
Fernando Lugo <lugo.fernando@gmail.com>
2021-02-06 04:20:59 -0800

    Add initial support for QEMU aarch64
    - Implement simple loader to allow PIE
    - Initialize .bss
    - Setup stack
    
    Signed-off-by: Fernando Lugo <lugo.fernando@gmail.com>

---
+++ src/board/qemu/aarch64/config.h
@@ -0,0 +1,11 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * (C) Copyright 2020, Fernando Lugo <lugo.fernando@gmail.com>
+ */
+
+#pragma once
+
+#define CONFIG_TEXT_BASE    0x40080000
+#define CONFIG_STACK_SIZE   (1024 * 16)
+#define CONFIG_HEAP_SIZE    (1024 * 32)
