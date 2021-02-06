775f8e8d339a7d6bdaceca744058fb13e1f4bb5c
Fernando Lugo <lugo.fernando@gmail.com>
2021-02-06 04:20:59 -0800

    libcxx: implement global new and delete operators
    Signed-off-by: Fernando Lugo <lugo.fernando@gmail.com>

---
+++ src/libcxx/new.cpp
@@ -0,0 +1,23 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright (c) 2020 Fernando Lugo <lugo.fernando@gmail.com>
+ */
+
+import lib.heap;
+
+#include <stddef.h>
+
+using sc::lib::heap::alloc;
+using sc::lib::heap::free;
+
+void* operator new(size_t size)
+{
+    // TODO: throw exception once they are available
+    return alloc(size);
+}
+
+void operator delete(void* p) noexcept { free(p); }
+void* operator new[](size_t size) { return operator new(size); }
+void operator delete[](void* p) noexcept { operator delete(p); }
+void* operator new(size_t size, void* p) { return p; }
