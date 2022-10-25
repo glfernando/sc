70450e45a3819f98b5fd9c6bd8be6c3799016543
Fernando Lugo <lugo.fernando@gmail.com>
2022-10-25 01:06:37 -0700

    aarch64: enable stack MTE support
    Compile with -fsanitize=memtag option
    
    Signed-off-by: Fernando Lugo <lugo.fernando@gmail.com>

--- make/arch/aarch64.mk
+++ make/arch/aarch64.mk
@@ -6,6 +6,7 @@ GLOBAL_CPPFLAGS += -DAARCH64 -target aarch64-unknown-none
 
 ifeq ($(CONFIG_AARCH64_MTE), y)
 GLOBAL_CPPFLAGS += -march=$(AARCH64_MARCH)+memtag
+GLOBAL_CPPFLAGS += -fsanitize=memtag
 GLOBAL_CPPFLAGS += -DCONFIG_AARCH64_MTE
 else
 GLOBAL_CPPFLAGS += -march=$(AARCH64_MARCH)
