0b730f2c89520df6aa36339140937e8e65eccba6
Fernando Lugo <lugo.fernando@gmail.com>
2022-10-27 01:34:29 -0700

    qemu-aarch64: include tests by default
    Signed-off-by: Fernando Lugo <lugo.fernando@gmail.com>

--- target/qemu-aarch64.mk
+++ target/qemu-aarch64.mk
@@ -4,5 +4,6 @@ BOARD_PATH := board/qemu/aarch64
 TARGET_CONFIG_FILE := board/qemu/aarch64/config.h
 CONFIG_PL011 := y
 CONFIG_CPU_SOC_DUMMY := y
+CONFIG_INCLUDE_TESTS := y
 
 include make/cpu/armv8.mk
